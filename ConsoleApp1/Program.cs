f252f7c5997dd402827dceebf40f3045bfd67529
VetalFV <novikov.1990.v.n@gmail.com>
2018-12-14 12:09:56 +0200

    1
    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press any key to continue...");
+
+            Console.ReadKey(true);
+        }
+    }
+
+
+}
