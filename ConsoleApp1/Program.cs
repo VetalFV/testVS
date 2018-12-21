e56382b1137c33a7444d73f34907dce117d711dd
VetalFV <novikov.1990.v.n@gmail.com>
2018-12-21 14:42:22 +0200

    6
    6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("Press any key to continue...");
+            Console.Write("Press any key to continue....");
 
             Console.ReadKey(true);
         }
